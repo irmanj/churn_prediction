d75e1236fb06c641675904b2c343b74c507572a5
irmanj <irma.nurjayanti@gmail.com>
2026-04-16 09:02:49 +0700

    add project churn prediction

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+
+df = pd.read_csv("data.csv")
+
+X = df[["umur", "langganan", "usage"]]
+y = df["churn"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+joblib.dump(model, "model.pkl")
